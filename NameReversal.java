77f1ebe8a2738b1c4d55e7128b91cf49139e7f9b
Sayan Raychaudhuri <Raychaudhurisayan@gmail.com>
2018-02-13 20:46:26 -0500

    Add files via upload

---
+++ NameReversal.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class NameReversal {
+
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	System.out.println("Please enter your name.");
+	String name = input.nextLine();
+	String lcname = name.toLowerCase();
+	for (int i=lcname.length()-1; i>=0;i--){
+		System.out.print(lcname.charAt(i));
+		}
+	}
+
+}
+/*
+import java.util.*;
+public class NameReversal {
+
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	System.out.println("Please enter your name.");
+	String name = input.nextLine();
+	for (int i=0; i<name.length();i++){
+		System.out.print(name.substring(name.length()-i-1,name.length()-i));
+		}
+	}
+
+}
+*/
